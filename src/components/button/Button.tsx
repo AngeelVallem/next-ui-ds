85e8ce9d30d10761b57b3442354bb9495717688f
Angel Valle <angeelvallem@gmail.com>
2024-01-16 20:58:16 -0600

    fix button imports

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,4 +1,4 @@
-import { classes } from "@/theme/components/button";
+import { tv } from "tailwind-variants";
 
 interface ButtonProps
   extends React.DetailedHTMLProps<
@@ -36,6 +36,30 @@ const Button = ({
   ref,
   ...props
 }: ButtonProps) => {
+  const classes = tv({
+    base: "rounded-md border border-solid py-sm outline-none duration-300 ease-linear active:scale-90",
+    variants: {
+      type: {
+        primary: "bg-primary text-white hover:opacity-80",
+        secondary: "border-secondary text-black hover:opacity-60",
+        text: "text-black hover:opacity-80",
+      },
+      shape: {
+        rounded: "rounded-xl",
+        normal: "rounded-md",
+        square: "rounded-none",
+      },
+      size: {
+        small: "px-xs text-sm",
+        medium: "px-xl",
+        large: "px-xxxl",
+      },
+      disabled: {
+        true: "pointer-events-none bg-accent3 opacity-50 active:transform-none",
+      },
+    },
+  });
+
   return (
     <button
       type="button"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import { colors, spacing } from "@/theme";
 
 const config: Config = {
   content: [
@@ -11,8 +10,29 @@ const config: Config = {
   darkMode: "class",
   theme: {
     extend: {
-      spacing,
-      colors,
+      spacing: {
+        xs: "0.375rem",
+        sm: "0.625rem",
+        md: "0.875rem",
+        xl: "1rem",
+        xxl: "1.25rem",
+        xxxl: "1.75rem",
+      },
+      colors: {
+        "ui-light": "#f5f5f5",
+        "ui-dark": "#091118",
+        primary: "#88c8b6",
+        "primary-h": "#79b5a4",
+        secondary: "#34bb92",
+        "secondary-h": "#2da681",
+        info: "#39cda9",
+        accent1: "#222631",
+        accent2: "#2f456a",
+        accent3: "#617396",
+        success: "#18ad41",
+        warning: "#f3c818",
+        danger: "#f60b3f",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
